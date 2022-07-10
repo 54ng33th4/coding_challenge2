8e9c57d835fb5e4b0858d49c2ddad246e202cd7c
Sangeetha S S <45419800+sang33th4@users.noreply.github.com>
2022-07-10 10:57:15 +0530

    v3

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-## mean-stack-blog
-
- Blogging website using the MEAN Stack (Mongo, Express, Angular, Node)
+# codingchallange2
--- frontend/mean-stack-blog/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://mean-stack-blog-api.etiennepuissant.eu',
-  clientUrl: 'https://mean-stack-blog.etiennepuissant.eu'
-};
